8915449e475cad8da49293e76eb4f339f69936a1
liren.lrl <liren.lrl@alibaba-inc.com>
2015-07-17 01:12:39 +0800

    add the libs

---
+++ src/main/java/org/roger/mylibrary/CommonPrint.java
@@ -0,0 +1,14 @@
+package org.roger.mylibrary;
+
+/**
+ * Created by liren on 15/7/17.
+ */
+public class CommonPrint {
+    public static final void printHello() {
+        System.out.println("Hello bintray world!");
+    }
+
+    public static void main(String[] args) {
+        CommonPrint.printHello();//it's not good.
+    }
+}
---
+++ src/main/java/org/roger/mylibrary/Dog.java
@@ -0,0 +1,13 @@
+package org.roger.mylibrary;
+
+/**
+ * Created by liren on 15/7/17.
+ */
+public class Dog {
+    public float length;
+    private String name;
+    public Dog() {
+        length = 10;
+        name = "Dick";
+    }
+}
